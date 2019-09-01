35132681d6a95dfc07311bcf0f24df7d7eaa7f7e
Bundit Jitkongchuen <boybundit@hotmail.com>
2019-09-01 13:57:39 +0700

    Add test case: Length(100cm) == Length(1m)

--- src/test/java/net/bundit/length/LengthTest.java
+++ src/test/java/net/bundit/length/LengthTest.java
@@ -47,4 +47,10 @@ public class LengthTest {
         Length one_m = new Length(1, LengthUnit.m);
         assertEquals(oneHundred_cm.toBaseUnit(), one_m);
     }
+    @Test
+    void given_length_100cm_when_compare_to_1m_then_it_should_be_equal() {
+        Length oneHundred_cm = new Length(100, LengthUnit.cm);
+        Length one_m = new Length(1, LengthUnit.m);
+        assertEquals(oneHundred_cm, one_m);
+    }
 }
